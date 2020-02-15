8f4a9a8ba5d12e9e0d974ee4e33aed65248ed70e
Antoine van Gelder <antoine@flowdsp.io>
2020-02-15 11:11:26 +0200

    Use a higher resolution timer for ledc pwm output (12 bits @ 19.531 kHZ)

--- api/src/ledc.rs
+++ api/src/ledc.rs
@@ -1,4 +1,22 @@
-use esp_idf::bindings as idf;
+use esp_idf::bindings::{
+    ESP_ERR_INVALID_ARG
+};
+use esp_idf::bindings::{
+    gpio_num_t,
+    ledc_channel_config_t,
+    ledc_channel_t,
+    ledc_mode_t,
+    ledc_timer_bit_t,
+    ledc_timer_config_t,
+    ledc_timer_t
+};
+use esp_idf::bindings::{
+    ledc_channel_config,
+    ledc_fade_func_install,
+    ledc_set_duty,
+    ledc_timer_config,
+    ledc_update_duty,
+};
 use esp_idf::{AsResult, EspError};
 
 use crate::logger;
@@ -8,52 +26,71 @@ use crate::logger;
 
 const TAG: &str = "api::ledc";
 
-const LEDC_BASE_FREQ: u32 = 50_000; // KHz
+// the lower the pwm signal frequency, the higher the duty resolution:
+//   40.00 MHz -> 1 bit
+//   20.00 MHz -> 2 bits
+//   10.00 MHz -> 3 bits
+//    5.00 MHz -> 4 bits
+//    2.50 MHz -> 5 bits
+//    1.25 MHz -> 6 bits
+//  625.00 kHz -> 7 bits
+//  312.50 kHz -> 8 bits
+//  156.25 kHz -> 9 bits
+//   78.13 kHz -> 10 bits
+//   39.06 kHz -> 11 bits
+//   19.53 kHz -> 12 bits
+//    9.76 kHz -> 13 bits
+//    4.88 kHz -> 14 bits
+//    2.44 kHz -> 15 bits
+//    1.22 kHz -> 16 bits
+//    ...
+const FREQ_HZ: u32 = 19_531;
+const DUTY_RESOLUTION: ledc_timer_bit_t = ledc_timer_bit_t::LEDC_TIMER_12_BIT;
+pub const DUTY_SCALE: f32 = ((2 << ((DUTY_RESOLUTION as u32) - 1)) - 1) as f32;
 
 
 // - exports ------------------------------------------------------------------
 
-pub unsafe fn init(pins: &[idf::gpio_num_t]) -> Result<(), EspError> {
-    if pins.len() >= idf::ledc_channel_t::LEDC_CHANNEL_MAX as usize {
-        log!(TAG, "Too many pins for ledc peripheral. Maximum allowed is: {:?}", idf::ledc_channel_t::LEDC_CHANNEL_MAX);
-        return Err(idf::ESP_ERR_INVALID_ARG.into());
+pub unsafe fn init(pins: &[gpio_num_t]) -> Result<(), EspError> {
+    if pins.len() >= ledc_channel_t::LEDC_CHANNEL_MAX as usize {
+        log!(TAG, "Too many pins for ledc peripheral. Maximum allowed is: {:?}",
+             ledc_channel_t::LEDC_CHANNEL_MAX);
+        return Err(ESP_ERR_INVALID_ARG.into());
     }
 
     log!(TAG, "initializing ledc pwm driver");
 
-    let timer = idf::ledc_timer_config_t {
-        duty_resolution: idf::ledc_timer_bit_t::LEDC_TIMER_8_BIT, // resolution of PWM duty cycle
-        freq_hz: LEDC_BASE_FREQ,                                  // frequency of PWM signal
-        speed_mode: idf::ledc_mode_t::LEDC_HIGH_SPEED_MODE,       // timer mode
-        timer_num: idf::ledc_timer_t::LEDC_TIMER_0,               // timer index
-        ..idf::ledc_timer_config_t::default()
+    let timer = ledc_timer_config_t {
+        timer_num: ledc_timer_t::LEDC_TIMER_0,          // timer index
+        speed_mode: ledc_mode_t::LEDC_HIGH_SPEED_MODE,  // timer mode
+        freq_hz: FREQ_HZ,                                    // frequency of PWM signal
+        duty_resolution: DUTY_RESOLUTION,                    // resolution of PWM duty cycle
+        ..ledc_timer_config_t::default()
     };
-    idf::ledc_timer_config(&timer);
+    ledc_timer_config(&timer);
 
     // configure ledc pwm channels
     log!(TAG, "configure pins for ledc pwm driver: {:?}", pins);
     for (channel, pin) in pins.iter().enumerate() {
-        let channel = core::mem::transmute::<usize, idf::ledc_channel_t>(channel);
-        let config = idf::ledc_channel_config_t {
+        let channel = core::mem::transmute::<usize, ledc_channel_t>(channel);
+        let config = ledc_channel_config_t {
             channel: channel,
             duty: 0,
             gpio_num: *pin as i32,
-            speed_mode: idf::ledc_mode_t::LEDC_HIGH_SPEED_MODE,
+            speed_mode: ledc_mode_t::LEDC_HIGH_SPEED_MODE,
             hpoint: 0,
-            timer_sel: idf::ledc_timer_t::LEDC_TIMER_0,
-            ..idf::ledc_channel_config_t::default()
+            timer_sel: ledc_timer_t::LEDC_TIMER_0,
+            ..ledc_channel_config_t::default()
         };
         log!(TAG, "configuring {:?} -> GPIO_{:?}", channel, pin);
-        idf::ledc_channel_config(&config);
+        ledc_channel_config(&config);
     }
 
-    idf::ledc_fade_func_install(0);
+    ledc_fade_func_install(0);
 
     // set default duty-cycle to 25%
     for (channel, _)  in pins.iter().enumerate() {
-        let channel = core::mem::transmute::<usize, idf::ledc_channel_t>(channel);
-        idf::ledc_set_duty(idf::ledc_mode_t::LEDC_HIGH_SPEED_MODE, channel, 64);
-        idf::ledc_update_duty(idf::ledc_mode_t::LEDC_HIGH_SPEED_MODE, channel);
+        update(channel as u8, 0.25);
     }
 
     Ok(())
@@ -61,10 +98,11 @@ pub unsafe fn init(pins: &[idf::gpio_num_t]) -> Result<(), EspError> {
 
 
 pub fn update(channel: u8, value: f32) {
-    let channel = unsafe { core::mem::transmute::<u32, idf::ledc_channel_t>(channel as u32) };
-    let duty = (value * 255.) as u32;
+    let channel = unsafe { core::mem::transmute::<u32, ledc_channel_t>(channel as u32) };
+    let duty = (value * DUTY_SCALE) as u32;
+
     unsafe {
-        idf::ledc_set_duty(idf::ledc_mode_t::LEDC_HIGH_SPEED_MODE, channel, duty);
-        idf::ledc_update_duty(idf::ledc_mode_t::LEDC_HIGH_SPEED_MODE, channel);
+        ledc_set_duty(ledc_mode_t::LEDC_HIGH_SPEED_MODE, channel, duty);
+        ledc_update_duty(ledc_mode_t::LEDC_HIGH_SPEED_MODE, channel);
     }
 }
