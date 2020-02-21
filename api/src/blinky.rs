c7ac62bfdd922312e8e8d3127cd9d82e0dc3cd23
Antoine van Gelder <antoine@flowdsp.io>
2020-02-21 16:20:38 +0200

    Use esp-idf API's for blinky implementation

--- api/src/blinky.rs
+++ api/src/blinky.rs
@@ -1,4 +1,16 @@
-use esp_idf::bindings::gpio_num_t;
+use esp_idf::{AsResult, EspError};
+use esp_idf::bindings::{
+    gpio_config_t,
+    gpio_mode_t,
+    gpio_num_t,
+    gpio_pulldown_t,
+    gpio_pullup_t,
+    gpio_int_type_t,
+};
+use esp_idf::bindings::{
+    gpio_config,
+    gpio_set_level,
+};
 
 use crate::logger;
 
@@ -21,7 +33,37 @@ mod gpio_reg {
 
 // - implementation -----------------------------------------------------------
 
-pub fn set_led(gpio: gpio_num_t, val: bool) {
+pub fn configure_pin_as_output(gpio: gpio_num_t) -> Result<(), EspError> {
+    log!(TAG, "configure pin for blinky: {:?}", gpio);
+    let config = gpio_config_t {
+        pin_bit_mask:  0x1 << (gpio as u32),
+        mode: gpio_mode_t::GPIO_MODE_OUTPUT,
+        pull_up_en: gpio_pullup_t::GPIO_PULLUP_DISABLE,
+        pull_down_en: gpio_pulldown_t::GPIO_PULLDOWN_DISABLE,
+        intr_type: gpio_int_type_t::GPIO_INTR_DISABLE,
+    };
+
+    unsafe { gpio_config(&config).as_result() }
+}
+
+
+pub fn configure_pin_as_output_raw(gpio: gpio_num_t) {
+    log!(TAG, "configure pin for blinky: {:?}", gpio);
+    unsafe {
+        core::ptr::write_volatile(gpio_reg::GPIO_ENABLE_W1TS_REG as *mut _, 0x1 << (gpio as u32));
+
+        let gpio_funcx_out_sel_cfg: u32 = gpio_reg::GPIO_FUNCX_OUT_BASE + ((gpio as u32) * 4);
+        core::ptr::write_volatile(gpio_funcx_out_sel_cfg as *mut _, 0x100);
+    }
+}
+
+
+pub fn set_led(gpio: gpio_num_t, val: bool) -> Result<(), EspError > {
+    unsafe { gpio_set_level(gpio, val as u32).as_result() }
+}
+
+
+pub fn set_led_raw(gpio: gpio_num_t, val: bool) {
     if val {
         unsafe {
             core::ptr::write_volatile(gpio_reg::GPIO_OUT_W1TS_REG as *mut u32, 0x1 << (gpio as u8));
@@ -30,17 +72,6 @@ pub fn set_led(gpio: gpio_num_t, val: bool) {
        unsafe {
             core::ptr::write_volatile(gpio_reg::GPIO_OUT_W1TC_REG as *mut u32, 0x1 << (gpio as u8));
         }
-    }
-}
-
-
-pub fn configure_pin_as_output(gpio: gpio_num_t) {
-    log!(TAG, "configure pin for blinky: {:?}", gpio);
-    unsafe {
-        core::ptr::write_volatile(gpio_reg::GPIO_ENABLE_W1TS_REG as *mut _, 0x1 << (gpio as u32));
-
-        let gpio_funcx_out_sel_cfg: u32 = gpio_reg::GPIO_FUNCX_OUT_BASE + ((gpio as u32) * 4);
-        core::ptr::write_volatile(gpio_funcx_out_sel_cfg as *mut _, 0x100);
     }
 }
 
