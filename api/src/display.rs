3c41ce7b9332e70b0a3fc62c9f5a873e2c1d271b
Antoine van Gelder <antoine@flowdsp.io>
2020-04-15 15:33:34 +0200

    Require callers to manage frame buffer memory for the display driver

--- api/src/display.rs
+++ api/src/display.rs
@@ -29,21 +29,25 @@ pub struct Config {
 }
 
 
-pub struct Interface<D> {
+pub struct Interface<'a, D> {
     pub config: Config,
     pub driver: D,
+    pub closure: Box<dyn FnMut(&mut Buffer) + 'a>,
+    pub frame_buffer: &'a mut [u8],
 }
 
 
-impl<D> Interface<D>
+impl<'a, D> Interface<'a, D>
 where D: driver::Display {
-    pub fn new(width: usize, height: usize) -> Interface<D> {
+    pub fn new<F: FnMut(&mut Buffer) + 'a>(width: usize, height: usize, frame_buffer: &'a mut [u8], closure: F) -> Interface<'a, D> {
         Interface {
             config: Config {
                 width: width,
                 height: height,
             },
             driver: D::new(),
+            closure: Box::new(closure),
+            frame_buffer: frame_buffer,
         }
     }
 
@@ -51,8 +55,13 @@ where D: driver::Display {
         self.driver.init(&mut self.config)
     }
 
-    pub fn write(&self, frame_buffer: &Buffer) -> Result<(), EspError> {
-        self.driver.write(frame_buffer)
+    pub fn refresh(&mut self) -> Result<(), EspError> {
+        (self.closure)(self.frame_buffer);
+        Ok(())
+    }
+
+    pub fn flush(&self) -> Result<(), EspError> {
+        self.driver.write(self.frame_buffer)
     }
 }
 
@@ -62,9 +71,9 @@ where D: driver::Display {
 // TODO move this to driver::sh1106 - chances are we'll have to do one
 // of these for each physical display we want to support
 
-const WIDTH: usize = 128;
-const HEIGHT: usize = 64;
-const PAGES: usize = 8;
+const WIDTH: usize = 128; // TODO lose
+const HEIGHT: usize = 64; // TODO lose
+const PAGES: usize = 8;   // TODO lose
 
 use embedded_graphics::{
     drawable::Pixel,
@@ -72,39 +81,18 @@ use embedded_graphics::{
     Drawing
 };
 
-#[derive(Copy, Clone, PartialEq, Debug)]
-pub struct CustomPixelColor {
-    pub value: u8,
+
+pub struct Display <'a> {
+    pub frame_buffer: &'a mut [u8],
 }
 
-impl CustomPixelColor {
-    fn new(color: u8) -> Self {
-        CustomPixelColor { value: color }
-    }
-}
-
-impl PixelColor for CustomPixelColor {}
-
-impl From<u8> for CustomPixelColor {
-    fn from(other: u8) -> Self {
-        CustomPixelColor {
-            value: other,
-        }
-    }
-}
-
-pub struct Display {
-    pub frame_buffer: [u8; WIDTH * PAGES],
-}
-
-impl Display {
-    pub fn new() -> Display {
+impl<'a> Display<'a> {
+    pub fn new(frame_buffer: &'a mut [u8]) -> Display {
         Display {
-            frame_buffer: [0; WIDTH * PAGES],
+            frame_buffer: frame_buffer,
         }
     }
 
-    // TODO figure out the 'right' way for the embedded_graphics crate
     pub fn clear(&mut self) -> () {
         for pixel in self.frame_buffer.iter_mut() {
             *pixel = 0;
@@ -112,7 +100,7 @@ impl Display {
     }
 }
 
-impl Drawing<CustomPixelColor> for Display {
+impl Drawing<CustomPixelColor> for Display<'_> {
     fn draw<T>(&mut self, item_pixels: T) where T: IntoIterator<Item = Pixel<CustomPixelColor>> {
         for Pixel(coord, color) in item_pixels {
             let x = coord[0] as usize;
@@ -132,6 +120,28 @@ impl Drawing<CustomPixelColor> for Display {
             } else {
                 self.frame_buffer[address] &= !(1 << (y % 8));
             }
+        }
+    }
+}
+
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub struct CustomPixelColor {
+    pub value: u8,
+}
+
+impl CustomPixelColor {
+    fn new(color: u8) -> Self {
+        CustomPixelColor { value: color }
+    }
+}
+
+impl PixelColor for CustomPixelColor {}
+
+impl From<u8> for CustomPixelColor {
+    fn from(other: u8) -> Self {
+        CustomPixelColor {
+            value: other,
         }
     }
 }
